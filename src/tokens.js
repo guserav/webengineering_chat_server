939319cee74d3adb8f625c8eb5b99380e6d4d983
guserav <erik.zeiske@web.de>
2018-02-22 15:55:58 +0700

    Implement token generation and validation
    Tokens are still auto accepted but the logic to verify them is implemented

--- src/tokens.js
+++ src/tokens.js
@@ -1,8 +1,30 @@
+const config = require('config.js');
+
+const jwt = require('jsonwebtoken');
+const TOKEN_EXPIRED_ERROR = "TokenExpiredError";
+const JWT_ERROR = "JsonWebTokenError";
+
 
 module.exports = {
     isValidToken: function(token){
-       console.log(new Date() + ' Auto accepted token: ' + token); //TODO remove logging of tokens
-       // TODO check token validity
+        try {
+            jwt.verify(token, config.jwt.decryptKey, config.jwt.verifyOptions);
+        } catch (error) {
+            if(error.name = TOKEN_EXPIRED_ERROR){
+                console.log(error);
+            } else if(error.name = JWT_ERROR){
+                console.log("Invalid token given: " + error);
+            } else {
+                console.error("Unexpected error while verifying token");
+                console.error(error);
+            }
+            //return false; TODO remove auto accept
+        }
+        console.log(new Date() + ' Auto accepted token: ' + token); //TODO remove logging of tokens
+        console.error("Token auto accepted");
         return true;
+    },
+    getNewToken: function(user){
+        return jwt.sign({user: user}, config.jwt.signKey, config.jwt.signOptions);
     }
 };
