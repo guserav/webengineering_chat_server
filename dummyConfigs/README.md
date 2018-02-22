b29735908676fcff26d0d50eec5b52a908ec14f7
guserav <erik.zeiske@web.de>
2018-02-22 15:53:22 +0700

    Start implementing extern configuration reading
    Implements reading of websockeckt configuration from config file

---
+++ dummyConfigs/README.md
@@ -0,0 +1,16 @@
+# Configs
+## config.json
+This config is the base config to specify the general configuration of the chatserver.
+The config has the following categories:
+##jwt
+Includes the all options related to JavascriptWebTokens.
+
+- signKeyLocation: The path of the file containing the signing key (Path is relative to /config)
+- decryptKeyLocation: The path of the file containing the decrypting key (Path is relative to /config)
+- signOptions: a object containing the options for [jwt.sign](https://github.com/auth0/node-jsonwebtoken#jwtsignpayload-secretorprivatekey-options-callback)
+- verifyOptions: a object containing the options for [jwt.verify](https://github.com/auth0/node-jsonwebtoken#jwtverifytoken-secretorpublickey-options-callback)
+
+
+##database
+All options for the database connection.
+WIP
---
+++ src/config.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+
+let config;
+
+try {
+    config = JSON.parse(fs.readFileSync(__dirname + '../config/config.json', 'utf8'));
+    //TODO: validate config errors
+} catch (error){
+    console.error('Error while parsing config.json');
+    throw error;
+}
+
+const signKey = fs.readFileSync(__dirname + 'config/' + config.jwt.signKeyLocation, 'utf8');
+const decryptKey = fs.readFileSync(__dirname + 'config/' + config.jwt.decryptKeyLocation, 'utf8');
+
+if(signKey === undefined || decryptKey === undefined){
+    throw new Error("Jwt keys are empty.");
+}
+
+console.log("Successfully parsed configuration.");
+module.exports = {
+    jwt:{
+        signKey: signKey,
+        decryptKey: decryptKey,
+        signOptions: config.jwt.signOptions,
+        verifyOptions: config.jwt.verifyOptions
+    }
+};
