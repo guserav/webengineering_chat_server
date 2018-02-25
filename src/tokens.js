2ea29a24f5e999baa624dd78e9fe5740d2eb62cc
guserav <erik.zeiske@web.de>
2018-02-25 22:05:31 +0700

    Remove auto accepting of tokens
    Also adding some comments for the token functions

--- src/tokens.js
+++ src/tokens.js
@@ -1,4 +1,4 @@
-const config = require('config.js');
+const config = require('./config.js');
 
 const jwt = require('jsonwebtoken');
 const TOKEN_EXPIRED_ERROR = "TokenExpiredError";
@@ -6,24 +6,36 @@ const JWT_ERROR = "JsonWebTokenError";
 
 
 module.exports = {
+    /**
+     * Checks if a token is valid.
+     * @param token {String} the jwt to check
+     * @returns {undefined | object} Representing the data in the jwt
+     */
     isValidToken: function(token){
+        let decodedToken;
         try {
-            jwt.verify(token, config.jwt.decryptKey, config.jwt.verifyOptions);
+            decodedToken = jwt.verify(token, config.jwt.decryptKey, config.jwt.verifyOptions);
         } catch (error) {
             if(error.name = TOKEN_EXPIRED_ERROR){
                 console.log(error);
             } else if(error.name = JWT_ERROR){
                 console.log("Invalid token given: " + error);
             } else {
-                console.error("Unexpected error while verifying token");
+                console.error(new Date() + "Unexpected error while verifying token");
                 console.error(error);
             }
-            //return false; TODO remove auto accept
+            return undefined;
         }
-        console.log(new Date() + ' Auto accepted token: ' + token); //TODO remove logging of tokens
-        console.error("Token auto accepted");
-        return true;
+        return decodedToken;
     },
+
+    /**
+     * Create a new Token for the given user.
+     *
+     * This method should only be called after the user has been verified.
+     * @param user The userIDs
+     * @returns {String}
+     */
     getNewToken: function(user){
         return jwt.sign({user: user}, config.jwt.signKey, config.jwt.signOptions);
     }
