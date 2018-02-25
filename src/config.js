ea5deca00f67d9485c2d2f5aef4d9289b7af5369
guserav <erik.zeiske@web.de>
2018-02-25 16:09:30 +0700

    Fix filepaths in config reading

--- src/config.js
+++ src/config.js
@@ -3,15 +3,15 @@ const fs = require('fs');
 let config;
 
 try {
-    config = JSON.parse(fs.readFileSync(__dirname + '../config/config.json', 'utf8'));
+    config = JSON.parse(fs.readFileSync(__dirname + '/../config/config.json', 'utf8'));
     //TODO: validate config errors
 } catch (error){
     console.error(new Date() + ' Error while parsing config.json');
     throw error;
 }
 
-const signKey = fs.readFileSync(__dirname + 'config/' + config.jwt.signKeyLocation, 'utf8');
-const decryptKey = fs.readFileSync(__dirname + 'config/' + config.jwt.decryptKeyLocation, 'utf8');
+const signKey = fs.readFileSync(__dirname + '/../config/' + config.jwt.signKeyLocation, 'utf8');
+const decryptKey = fs.readFileSync(__dirname + '/../config/' + config.jwt.decryptKeyLocation, 'utf8');
 
 if(signKey === undefined || decryptKey === undefined){
     throw new Error(new Date() + " Jwt keys are empty.");
