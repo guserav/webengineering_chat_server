acb6d7dc2bf11287234f6708d354418532406ad2
guserav <erik.zeiske@web.de>
2018-03-07 19:21:03 +0700

    Add check of database connection at server startup

--- app.js
+++ app.js
@@ -4,67 +4,82 @@ const http = require('http');
 const websocketLogic = require('./src/websockets');
 const express = require('express');
 const httpApi = require('./src/httpApi.js');
-const mysql      = require('mysql');
+const mysql = require('mysql');
+const mysqlTools = require('./src/mysql.js');
 
 // Initiate Database
 const pool = mysql.createPool(Object.assign({multipleStatements:true}, config.database));
 if(!pool) console.error(new Date() + "Failed to establish database connection!");
-// TODO check if database is ready
+mysqlTools.query(pool, "SELECT * FROM `user_room` LIMIT 1;SELECT * FROM `user` LIMIT 1;SELECT * FROM `room` LIMIT 1;").then(function(){
+    console.log(new Date() + " Database available.");
+    // HTTP Api init
+    const app = express();
+    httpApi.setDatabaseConnectionPool(pool);
+    httpApi.initialiseApp(app);
+    const server = http.createServer(app);
 
-// HTTP Api init
-const app = express();
-httpApi.setDatabaseConnectionPool(pool);
-httpApi.initialiseApp(app);
-const server = http.createServer(app);
-
-server.listen(config.http.port, function() {
-    const host = server.address().address;
-    const port = server.address().port;
-    console.log((new Date()) + ' Server is running on ' + host + ' and listening on port ' + port);
-});
+    server.listen(config.http.port, function() {
+        const host = server.address().address;
+        const port = server.address().port;
+        console.log((new Date()) + ' Server is running on ' + host + ' and listening on port ' + port);
+    });
 
 
 
-// Websocket initiation
-websocketLogic.setDatabaseConnectionPool(pool);
-const wsServer = new WebSocketServer({
-    httpServer: server,
-    autoAcceptConnections: false
-});
+    // Websocket initiation
+    websocketLogic.setDatabaseConnectionPool(pool);
+    const wsServer = new WebSocketServer({
+        httpServer: server,
+        autoAcceptConnections: false
+    });
 
-/**
- * Tests if the origin of the request can be accepted
- * @param request
- * @returns {boolean}
- */
-function isRequestAllowed(request) {
-    const toAccept = config.websocket.acceptOrigin;
-    if(toAccept){
-        if(!request.order) return false;
-        if(toAccept.length){ // Go through array of origins
-            for(let i = 0; i < toAccept.length; i++){
-                if(toAccept[i] === request.origin){
-                    return true;
+    /**
+     * Tests if the origin of the request can be accepted
+     * @param request
+     * @returns {boolean}
+     */
+    function isRequestAllowed(request) {
+        const toAccept = config.websocket.acceptOrigin;
+        if(toAccept){
+            if(!request.order) return false;
+            if(toAccept.length){ // Go through array of origins
+                for(let i = 0; i < toAccept.length; i++){
+                    if(toAccept[i] === request.origin){
+                        return true;
+                    }
                 }
+                return false; //No acceptable origin found
+            } else {
+                return toAccept === request.origin;
             }
-            return false; //No acceptable origin found
         } else {
-            return toAccept === request.origin;
+            console.log(new Date() + ' Origin auto accepted: ', request.origin);
+            return true;
         }
+    }
+
+    wsServer.on('request', function(request) {
+        if (!isRequestAllowed(request)) { //Deny non allowed origins
+            request.reject();
+            console.log((new Date()) + ' Connection from ', request.origin, ' rejected.');
+            return;
+        }
+
+        let connection = request.accept(null, request.origin); //specify protocol with none
+        console.log((new Date()) + ' Connection accepted.');
+        websocketLogic.setupConnection(connection);
+    });
+
+}).catch(function(err){
+    if(err.fatal){
+        console.error(new Date() + " Failed to connect to Database", err);
     } else {
-        console.log(new Date() + ' Origin auto accepted: ', request.origin);
-        return true;
+        mysqlTools.resetDatabase(pool, function(err, result){
+            if(err){
+                console.error(new Date() + "Failed to reset Database", err);
+            } else {
+                console.log(new Date() + " Reset Database because it was empty.", result);
+            }
+        });
     }
-}
-
-wsServer.on('request', function(request) {
-    if (!isRequestAllowed(request)) { //Deny non allowed origins
-        request.reject();
-        console.log((new Date()) + ' Connection from ', request.origin, ' rejected.');
-        return;
-    }
-
-    let connection = request.accept(null, request.origin); //specify protocol with none
-    console.log((new Date()) + ' Connection accepted.');
-    websocketLogic.setupConnection(connection);
 });
