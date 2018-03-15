42950cf11b6e7a4b4dada8b8a9f69096cb2f53d8
guserav <erik.zeiske@web.de>
2018-03-15 15:40:49 +0700

    Rewrite http API to use Promise wrapper

--- src/httpApi.js
+++ src/httpApi.js
@@ -31,64 +31,63 @@ module.exports = {
         app.post("/user/newToken",async function(req, res){
             const password = req.body.password;
             const user = req.body.user;
+            try{
+                const results = await mysql.query(
+                    _this.databaseConPool,
+                    'SELECT * FROM `user` WHERE `user`.`userID` = ?',
+                    [user]
+                );
 
-            _this.databaseConPool.getConnection(function(err, connection){
-                if(err) throw err;
-                connection.query('SELECT * FROM `user` WHERE `user`.`userID` = ?', [user], function(err, results, fields){
-                    connection.release();
-                    if(err){
-                        console.error(err);
-                        res.sendStatus(500);
-                        return;
-                    }
-                    if(results.length !== 1){
-                        res.status(403).send("Username or password not correct");
-                        return;
-                    }
-                    const lookupPassword = results[0].passwordHash;
-                    const salt = results[0].salt;
+                if(results.length !== 1){
+                    res.status(403).send("Username or password not correct");
+                    return;
+                }
+                const lookupPassword = results[0].passwordHash;
+                const salt = results[0].salt;
 
-                    const computedPassword = scrypt.hashSync(password, salt).toString('base64');
+                const computedPassword = scrypt.hashSync(password, salt).toString('base64');
 
-                    if(computedPassword !== lookupPassword){
-                        res.status(403).send("Username or password not correct");
-                        return;
-                    }
-                    res.status(200).send(token.getNewToken(user));
-                });
-            });
+                if(computedPassword !== lookupPassword){
+                    res.status(403).send("Username or password not correct");
+                    return;
+                }
+                res.status(200).send(token.getNewToken(user));
+            } catch (err){
+                console.error(new Date() + err);
+                res.sendStatus(500);
+            }
         });
 
         app.post("/user/create",async function(req, res){
-            const password = req.body.password;
-            const user = req.body.user;
+            try {
+                const password = req.body.password;
+                const user = req.body.user;
 
-            if(password === undefined || user === undefined){
+                if (password === undefined || user === undefined) {
+                    res.sendStatus(403);
+                    return;
+                }
+
+                let saltArray = [];
+                for (let i = 0; i < 32; i++) {
+                    let randomInt = await randomNumber(-Math.pow(2, 8 * 2), Math.pow(2, 8 * 2));
+                    saltArray.push(randomInt);
+                }
+
+                const salt = Buffer.from(saltArray).toString('base64');
+                const hash = scrypt.hashSync(password, salt).toString('base64');
+
+                await mysql.query(
+                    _this.databaseConPool,
+                    'INSERT INTO `user`(`userID`, `passwordHash`, `salt`) VALUES (? , ?, ?)',
+                    [user, hash, salt]
+                );
+
+                res.sendStatus(200);
+            } catch (err){
+                console.error(new Date() + err);
                 res.sendStatus(403);
-                return;
             }
-
-            let saltArray = [];
-            for (let i = 0; i < 32; i++){
-                let randomInt = await randomNumber(-Math.pow(2, 8 * 2), Math.pow(2, 8 * 2));
-                saltArray.push(randomInt);
-            }
-
-            const salt = Buffer.from(saltArray).toString('base64');
-            const hash = scrypt.hashSync(password, salt).toString('base64');
-
-            _this.databaseConPool.getConnection(function(err, connection){
-                if(err) throw err;
-                connection.query('INSERT INTO `user`(`userID`, `passwordHash`, `salt`) VALUES (? , ?, ?)', [user, hash, salt], function(err, results, fields) {
-                    // TODO add detailed answer
-                    connection.release();
-                    if(err){
-                        res.sendStatus(403);
-                        return;
-                    }
-                    res.sendStatus(200);
-                });
-            });
         });
 
         if(config.debug === true){
@@ -109,7 +108,9 @@ module.exports = {
                         return;
                     }
                     console.log(new Date() + "Successfully reset database.");
-                    res.status(200).send("Successfully performed query.\n" + JSON.stringify(result));
+                    res.status(200);
+                    writeObjectToWebpage(res, result);
+                    res.end();
                 });
             });
 
